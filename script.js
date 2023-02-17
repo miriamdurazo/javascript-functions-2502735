0718adb369f93e9958181c5eec775ad2523f2718
miriamdurazo <119426793+miriamdurazo@users.noreply.github.com>
2023-02-17 20:21:57 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-async function output(data) {
-    const $ = document.querySelector.bind(document)
-    let output = '<nav><ul>'
-    for (const key in data) {
-        output +=`<li><a class="bi-${key}" href="${data[key]}"> ${key}</a>`
-    }
-    output +='</ul></nav>'
-    $('#output').innerHTML = output
+// async function output(data) {
+//     const $ = document.querySelector.bind(document)
+//     let output = '<nav><ul>'
+//     for (const key in data) {
+//         output +=`<li><a class="bi-${key}" href="${data[key]}"> ${key}</a>`
+//     }
+//     output +='</ul></nav>'
+//     $('#output').innerHTML = output
+// }
+
+// async function getSocial() {
+//     const res = await fetch('./social.json')
+//     const data = await res.json()
+//     return data
+// }
+
+// async function doSocial() {
+//     const social = await getSocial()
+//     await output(social)
+// }
+
+// doSocial();
+
+info = {
+  social: {
+    youtube: 'https://youtube.com/planetoftheweb',
+    linkedin: 'https://linkedin.com/in/planetoftheweb',
+    blog: 'https://raybo.org'
+  }
 }
 
-async function getSocial() {
-    const res = await fetch('./social.json')
-    const data = await res.json()
-    return data
+function listObject(data) {
+    
 }
 
-async function doSocial() {
-    const social = await getSocial()
-    await output(social)
-}
-
-doSocial();
+listObject(info.social)
